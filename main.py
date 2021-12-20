0ac869df36cb7ad6c3bec18253e1cb3cabbe175f
ajsya <52182942+ajsya@users.noreply.github.com>
2021-12-20 16:19:44 -0500

    added random location option

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import scratchconnect
 from dotenv import load_dotenv
 import os
 import time
+import random
 
 load_dotenv()
 
@@ -13,7 +14,7 @@ username = os.environ['USERNAME']
 password = os.environ['PASSWORD']
 
 user = scratchconnect.ScratchConnect(username, password)
-project = user.connect_project(619628679)  # Connect the project
+project = user.connect_project(596980037)  # Connect the project
 
 def getWeather(city):
     api = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&units=imperial&appid="+API_KEY
@@ -42,7 +43,7 @@ def getWeather(city):
 
         return "Location Not Found"
 
-#variables = ['location', 'condition', 'description', 'temp', 'feels like temp', 'max temp', 'min temp', 'humidity', 'cloud coverage']
+RandomLocations = ['Toledo', 'Detriot', 'Columbus', 'Cleveland', 'Miami', 'Olympia', 'Washington DC', 'Boston', 'Kansas City', 'Orlando']
 while True:
     variables = project.connect_cloud_variables()
     request = variables.get_cloud_variable_value(variable_name='request')[0]
@@ -50,6 +51,19 @@ while True:
     if request != '1':
         if request == '2':
             print('No new requests.')
+        elif request == '3':
+            print("New request intetified; Random Location.")
+            weather = getWeather(random.choice(RandomLocations))
+            print(weather)
+            if weather == "Location Not Found":
+                variables.set_cloud_variable(variable_name='request', value='2')
+                print("Response submited: Location not Found; Code 2")
+            else:
+                encoded = variables.encode_list(list(weather))
+                variables.set_cloud_variable(variable_name='response', value=encoded)
+
+                variables.set_cloud_variable(variable_name='request', value='1')
+                print("Response Submited: Date sent; Code 1")
         else:    
             print("New request intetified.")
             location = variables.decode(request)
